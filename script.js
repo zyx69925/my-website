bbf61d6567502f68187e4506f91f08b2aa699db7
zyx69925 <164844932+zyx69925@users.noreply.github.com>
2025-11-11 21:39:21 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
   const btn = document.getElementById('greetBtn');
   const result = document.getElementById('greetResult');
 
+  // 定义一个计数器函数，从42开始
+  let count = 1;
+  function increaseCount() {
+    count++;
+    return count;
+  }
+
   btn.addEventListener('click', function() {
-    const now = new Date();
-    const timeStr = now.toLocaleTimeString();
-    result.textContent = `王昱娇你在干嘛呀！有没有想我 — 当前时间 ${timeStr}，祝你今天愉快！`;
+    const currentCount = increaseCount(); // 每次调用都会 +1
+    result.textContent = `我也想你了x ${currentCount} 嘻嘻！ (˃͈ દ ˂͈ ༶ ) `;
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('greetBtn2');
+  const result = document.getElementById('greetResult');
+
+  // 定义一个计数器函数，从42开始
+  let count = 1;
+  function increaseCount() {
+    count++;
+    return count;
+  }
+
+  btn.addEventListener('click', function() {
+    const currentCount = increaseCount(); // 每次调用都会 +1
+    result.textContent = `那我也不要想你了 呜呜！大坏蛋x ${currentCount} `;
   });
 });
